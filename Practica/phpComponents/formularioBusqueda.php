01f84a676809e2efd8bc5fdcddfe31eeaa8185ee
JesusGlez13 <99421497+JesusGlez13@users.noreply.github.com>
2023-04-18 17:22:53 +0200

    Actualizando el formulario de búsqueda

--- Practica/phpComponents/formularioBusqueda.php
+++ Practica/phpComponents/formularioBusqueda.php
@@ -1,57 +1,56 @@
 <form action="" method="post">
-<!-- <form action="formularioBusqueda.php" method="post"> for Testing -->
+    <div class="input-group mb-3">
+        <div class="input-group-prepend">
+            <button class="btn btn-outline-secondary" type="button" data-toggle="collapse" data-target="#collapseFilter" aria-expanded="false" aria-controls="collapseFilter">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+        <input type="text" class="form-control" placeholder="Search by name" name="nombre" value="<?php echo isset($_POST['nombre']) ? $_POST['nombre'] : ''; ?>">
+        <div class="input-group-append">
+            <button class="btn btn-outline-secondary" type="submit">Search</button>
+        </div>
+    </div>
 
-    <?php include_once("funcionBBDD.php");
-   
-            $link = db_connect();
-            $qry_generos = "SELECT * FROM generos";
-            $rsc_generos = peticionSQL($qry_generos,$link);
-            if($rsc_generos){
-                echo "<ul>";
-            while ($genero = mysqli_fetch_object($rsc_generos)) {
-            
-                ?>
-                
-                <li><?=$genero->nombreGenero?><input type="checkbox" name="genero[]" id="genero" value="<?=$genero->idGenero?>" 
-                
+    <div class="collapse" id="collapseFilter">
+        <div class="card card-body">
+            <h4>Filter by category:</h4>
+            <div class="form-group">
                 <?php
-                if (isset($_POST["genero"]) && in_array($genero->idGenero, $_POST['genero'])) {
-                    echo("checked");
-                  } 
-                ?>></li>
-<?php
-            }
-            echo "</ul>";
-        }
-        $qry_consolas = "SELECT * FROM consolas";
-        $rsc_consolas = peticionSQL($qry_consolas,$link);
-        if($rsc_consolas){
-            echo "<ul>";
-        while ($consola = mysqli_fetch_object($rsc_consolas)) {
-            ?>
-            <li><?=$consola->nombreConsola?><input type="checkbox" name="consola[]" id="consola" value="<?=$consola->idConsola?>" 
-            <?php
-             if (isset($_POST["consola"]) && in_array($consola->idConsola, $_POST['consola'])) {
-                    echo("checked");
-                  } 
-            ?>></li>
-    <?php
-    
-                }
-                echo "</ul>";
-            }
-    ?>
-
-            <ul> <li>Nombre Juego <input type="text" placeholder="Busqueda.." name="nombre"
-            
-            <?php
-             if (isset($_POST["nombre"])) {
-                    echo " value = \"".$_POST["nombre"]."\"";
-                  } 
-            ?>
-            
-            > </li> </ul>
-
-    <input type="submit" value="submit">
-
+                    $link = db_connect();
+                    $qry_generos = "SELECT * FROM generos";
+                    $rsc_generos = peticionSQL($qry_generos, $link);
+                    if ($rsc_generos) {
+                        while ($genero = mysqli_fetch_object($rsc_generos)) {
+                            echo "<div class='form-check'>";
+                            echo "<input class='form-check-input' type='checkbox' name='genero[]' id='genero{$genero->idGenero}' value='{$genero->idGenero}'";
+                            if (isset($_POST["genero"]) && in_array($genero->idGenero, $_POST['genero'])) {
+                                echo " checked";
+                            }
+                            echo ">";
+                            echo "<label class='form-check-label' for='genero{$genero->idGenero}'>{$genero->nombreGenero}</label>";
+                            echo "</div>";
+                        }
+                    }
+                ?>
+            </div>
+            <div class="form-group">
+                <?php
+                    $qry_consolas = "SELECT * FROM consolas";
+                    $rsc_consolas = peticionSQL($qry_consolas, $link);
+                    if ($rsc_consolas) {
+                        while ($consola = mysqli_fetch_object($rsc_consolas)) {
+                            echo "<div class='form-check'>";
+                            echo "<input class='form-check-input' type='checkbox' name='consola[]' id='consola{$consola->idConsola}' value='{$consola->idConsola}'";
+                            if (isset($_POST["consola"]) && in_array($consola->idConsola, $_POST['consola'])) {
+                                echo " checked";
+                            }
+                            echo ">";
+                            echo "<label class='form-check-label' for='consola{$consola->idConsola}'>{$consola->nombreConsola}</label>";
+                            echo "</div>";
+                        }
+                    }
+                ?>
+            </div>
+        </div>
+    </div>
 </form>
