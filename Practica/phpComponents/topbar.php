538c53dc5732870754b16a29fbe71ac3c27e5a4e
roxoner44 <drosique6@alu.ucam.edu>
2023-04-18 17:11:42 +0200

    modificado topbar para que se muestre el perfil

--- Practica/phpComponents/topbar.php
+++ Practica/phpComponents/topbar.php
@@ -19,13 +19,14 @@
     <!--Enlaces de sesion-->
     <?php if(isset($_SESSION['user'])){
         $funcion = "javascript:void(cerrarSesion())";
+         echo "<a href=\"#\" class=\"split\"><i class=\"fa-solid fa-circle-user fa-2xl\" style=\"color: #000000;\"></i></a> ";
     }else{
         $funcion ="javascript:void(document.getElementById('id01').style.display='block')";
     }
     ?>
 
     
-    <a href="#" class="split"><i class="fa-solid fa-circle-user fa-2xl" style="color: #000000;"></i></a> 
+   
     <!--Falta el enlace hacia el perfil^-->
 
 
