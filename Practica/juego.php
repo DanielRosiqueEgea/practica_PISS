56b9e82996ef19c109e706c585c55c3e404e24b2
Alexevers <alexandroillan@gmail.com>
2023-04-19 16:36:32 +0200

    Popup instrucciones y historial
    he metido un popuo para las instrucciones y otro para el historial de partidas

--- Practica/juego.php
+++ Practica/juego.php
@@ -20,6 +20,7 @@
 
 
 <link rel="stylesheet" type="text/css" href="estilos/juego.css">
+<link rel="stylesheet" type="text/css" href="estilos/popup_reglas.css">
 </head>
 
 <body>
@@ -128,8 +129,11 @@
         }
         $juego = mysqli_fetch_object($qry_juego);
         ?>
+    
         <article class="gameContainer">
 
+
+        
             <div id="carouselId" class="carousel slide" data-ride="carousel">
                 <?PHP
                 $sql_imagenes = "SELECT * FROM `imagenes` WHERE idJuego=" . $juego->idJuego;
@@ -168,8 +172,65 @@
         </article>
         <br>
         <h1>JUEGO</h1>
-        <article class="videoContainer">
 
+        <!--BOTON HISTORIAL-->
+        
+        <!--BOTON INSTRUCCIONES-->
+        <div class="boton-modal">
+            <label for="btn-modal-instrucciones">
+            <span class="icon"> <img src="imagenes\instrucciones.png" alt=""></span>    
+            
+            </label>
+
+            <label for="btn-modal-historial">
+            <span class="icon"> <img src="imagenes\historial.png" alt=""></span>    
+            
+            </label>
+        </div>
+        <!--Fin de Boton-->
+
+        <!--Ventana Modal INSTRUCCIONES-->
+        <input type="checkbox" id="btn-modal-instrucciones">
+        <div class="container-modal">
+            <div class="content-modal">
+                <h2>Instrucciones básicas del juego</h2>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                     Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                     Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                <div class="btn-cerrar">
+                    <label for="btn-modal-instrucciones">Cerrar</label>
+                </div>
+            </div>
+            <label for="btn-modal-instrucciones" class="cerrar-modal"></label>
+        </div>
+        <!--Fin de Ventana Modal INSTRUCCIONES-->
+
+        <!--Ventana Modal HISTORIAL-->
+        <input type="checkbox" id="btn-modal-historial">
+                <div class="container-modal-historial">
+                    <div class="content-modal-historial">
+                        <h2>Historial de la partida</h2>
+                        <p>Jugador 1: movimiento x       <b>12:05</b>
+                        <br>Jugador 2: movimiento y      <b>12:15</b><br>
+                        Jugador 1: movimiento x+1        <b>12:25</b>
+                        <br>Jugador 2: movimiento y+1    <b>12:34</b><br>
+                        Jugador 1: movimiento x+2        <b>12:36</b>
+                        <br>Jugador 2: movimiento y+2    <b>12:43</b><br>
+                        Jugador 1: movimiento x+3        <b>12:46</b>
+                        <br>Jugador 2: movimiento y+3    <b>12:58</b><br>
+                        </p>
+                        <div class="btn-cerrar">
+                            <label for="btn-modal-historial">Cerrar</label>
+                        </div>
+                    </div>
+                    <label for="btn-modal-historial" class="cerrar-modal"></label>
+                </div>
+                <!--Fin de Ventana Modal HISTORIAL-->
+
+        <!--Vista del juego-->
+        <article class="videoContainer">
+        
 
             <?php if ($juego->trailer != "not available") { ?>
 
@@ -196,12 +257,14 @@
 
         
         </article>
+
+       
     </section>
 
-                
- 
 
+   
 
 </body>
+
 
 </html>
